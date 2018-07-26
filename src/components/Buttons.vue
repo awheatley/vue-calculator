b5421b6ec7ba72df8bf23c0589673cbb9681a9a4
Adam Wheatley <adam.wheatley@caddis.co>
2018-07-26 04:56:14 -0500

    Create Buttons component

---
+++ src/components/Buttons.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <button v-for="(button, key) in buttons" :key="button.id">{{ key }}</button>
+        <button>.</button>
+        <button v-for="(operator, key) in operators" :key="operator.id">{{ key }}</button>
+        <button>=</button>
+        <button>C</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: ['buttons', 'operators'],
+    }
+</script>
