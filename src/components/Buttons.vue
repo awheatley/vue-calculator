bfa172b3d7df86b3cf9a5ecd462127608d2a2b73
Adam Wheatley <adam.wheatley@caddis.co>
2018-07-28 09:25:18 -0500

    Add button styles

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -13,4 +13,69 @@
         name: 'Button',
         props: ['buttons', 'operators'],
     }
-</script>
+</script><style lang="scss">
+    .calculator {
+        &__button {
+            border: none;
+            border-radius: 50%;
+            color: white;
+            cursor: pointer;
+            display: flex;
+            font-family: 'Avenir', Helvetica, sans-serif;
+            font-size: 2rem;
+            height: 5rem;
+            justify-content: center;
+            margin: 1rem;
+            transition: color 0.3s ease;
+            width: 5rem;
+            position: relative;
+            z-index: 1;
+
+            &::before,
+            &::after {
+                border-radius: inherit;
+                content: '';
+                height: 100%;
+                left: 0;
+                position: absolute;
+                top: 0;
+                transition: box-shadow 0.3s ease, transform 0.3s ease;
+                width: 100%;
+                z-index: -1;
+            }
+
+            &::before {
+                box-shadow: inset 0 0 0 0 red;
+            }
+
+            &::after {
+                box-shadow: inset 0 0 0 2.5rem red;
+            }
+
+            &:hover {
+                color: red;
+                &::after {
+                    box-shadow: inset 0 0 0 0.15rem red;
+                    transform: scale3d(1.2, 1.2, 1.2);
+                }
+            }
+
+            &:focus {
+                outline: none;
+            }
+
+            &.active {
+                color: red;
+
+                &::before {
+                    box-shadow: inset 0 0 0 0.15rem red;
+                }
+
+                &::after {
+                    box-shadow: inset 0 0 0 0.15rem red;
+                    transform: scale3d(1.2, 1.2, 1.2);
+                }
+            }
+        } 
+    }
+</style>
