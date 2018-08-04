786c3e74a466756ad0dab9fcbd2c79a501e6c42b
Adam Wheatley <adam.wheatley@caddis.co>
2018-08-04 17:50:22 -0500

    Add data properties to App.vue

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,13 @@ export default {
     return {
       buttons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
       operators: ['+', '-', '*', '/']
+      operators: ['+', '-', '*', '/'],
+      calculation: 0,
+      operator: null,
+      val1Num: 0,
+      val2Num: 0,
+      val1Str: '',
+      val2Str: '',
     }
   }
 }
